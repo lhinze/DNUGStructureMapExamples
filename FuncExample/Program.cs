439d1f1f813a01973788bb6a2d0792e8279684c3
lhinze <hinzel87@gmail.com>
2015-05-20 16:29:21 -0500

    Adding Lazy and Func examples.

---
+++ FuncExample/Program.cs
@@ -0,0 +1,66 @@
+﻿using StructureMap;
+using System;
+
+namespace FuncExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var container = new Container(
+                x =>
+                {
+                    x.For<IFoo>().Use<Foo>();
+                    x.For<IBar>().Use<Bar>();
+                });
+
+            var foo = container.GetInstance<IFoo>();
+
+            var bar = foo.DoSomething(2);
+
+            bar.PrintValue();
+
+            Console.ReadLine();
+        }
+
+        public interface IFoo
+        {
+            IBar DoSomething(int value);
+        }
+
+        public class Foo : IFoo
+        {
+            private readonly Func<int, IBar> _FuncBar;
+
+            public Foo(Func<int, IBar> funcBar)
+            {
+                _FuncBar = funcBar;
+            }
+
+            public IBar DoSomething(int value)
+            {
+                var bar = _FuncBar.Invoke(value);
+                return bar;
+            }
+        }
+
+        public interface IBar
+        {
+            void PrintValue();
+        }
+
+        public class Bar : IBar
+        {
+            private readonly int _Value;
+            public Bar(int value)
+            {
+                _Value = value;
+            }
+
+            public void PrintValue()
+            {
+                Console.WriteLine("Value is: {0}", _Value);
+            }
+        }
+    }
+}
---
+++ LazyExample/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using StructureMap;
+
+namespace LazyExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var container = new Container(
+                x =>
+                {
+                    x.For<IFoo>().Use<Foo>();
+                    x.For<IBar>().Use<Bar>();
+                });
+
+            var foo = container.GetInstance<IFoo>();
+
+            Console.WriteLine("Press ENTER to DoSomething with Foo");
+            Console.ReadLine();
+
+            foo.DoSomething();
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IFoo
+    {
+        void DoSomething();
+    }
+
+    public class Foo : IFoo
+    {
+        private readonly Lazy<IBar> _LazyBar;
+
+        public Foo(Lazy<IBar> lazyBar)
+        {
+            _LazyBar = lazyBar;
+        }
+
+        public void DoSomething()
+        {
+            _LazyBar.Value.Test();
+        }
+    }
+
+    public interface IBar
+    {
+        string Test();
+    }
+
+    public class Bar : IBar
+    {
+        public Bar()
+        {
+            Console.WriteLine("Bar has been constructed.");
+        }
+
+        public string Test()
+        {
+            return string.Empty;
+        }
+    }
+}
